cf58e3dd652fb5279c47a16d4bd7ac19c7db009f
Gonzalo Radovitzky <gonzardvtzky@gmail.com>
2023-11-02 18:41:11 -0300

    Se agrega caja de comentarios en sección de rating
    además de un pop-up de "función deshabilitada" al intentar comprar.

---
+++ scripts/unavailable.js
@@ -0,0 +1,5 @@
+const btn = document.querySelector(".comprar")
+
+btn.addEventListener("click", () => {
+    alert("Función no disponible actualmente. Las compras online serán incorporadas en un futuro.")
+})
